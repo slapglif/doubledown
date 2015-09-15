58fbcee79341e161e10daa2ebebe1246cf01a28d
kuro112 <kuro@xtcr.net>
2015-09-15 13:25:28 -0700

    Second Commit

--- untitled1.py
+++ untitled1.py
@@ -7,6 +7,11 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/user')
+def user():
+    return render_template("user.html")
+
+
 
 if __name__ == '__main__':
     app.run(debug=True,host='127.0.0.1',threaded=True)
